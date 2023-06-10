fcd97840bf1ca64af8dd730e1e4546df5d2cee3c
achrafbsibiss <achraf.bsibiss11@gmail.com>
2023-06-10 11:36:12 +0100

    Removing User and Fact

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_28_111451) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "facts", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "fact"
-    t.integer "likes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_facts_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "facts", "users"
 end
